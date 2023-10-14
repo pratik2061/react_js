ef8f4f432816691a907837c157d2620eddd5a17c
pratik2061 <123287083+pratik2061@users.noreply.github.com>
2023-10-14 09:46:41 +0545

    react_js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from './hello';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div style={{fontSize: "50px"}}>
+  <h2 style={{textAlign:'center'}}>It's pratik</h2>
+</div>
+ 
   );
 }
 
